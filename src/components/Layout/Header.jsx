f328ec63f963544466e97a27ca61d668e30bed2d
Enrique Padron Spångberg <enrique.padron853@gmail.com>
2023-10-22 15:37:45 -0400

    Initial commit

---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,35 @@
+import { Link } from "react-router-dom";
+import Logo from "../../assets/logo.png";
+
+export const Header = () => {
+  return (
+    <header>
+      <nav className="bg-white dark:bg-gray-900">
+        <div className="border-b border-slate-200 dark:border-b-0 flex flex-wrap justify-between items-center mx-auto max-w-screen-xl px-4 md:px-6 py-3">
+          <Link to="/" className="flex items-center">
+            <img
+              src={Logo}
+              className="mr-3 w-40"
+              alt="Chapter One: Books Store"
+            />
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+              Chapter One: Books Store
+            </span>
+          </Link>
+          <div className="flex items-center relative">
+            <span className="cursor-pointer text-xl text-gray-700 dark:text-white mr-5 bi bi-gear-wide-connected"></span>
+            <span className="cursor-pointer text-xl text-gray-700 dark:text-white mr-5 bi bi-search"></span>
+            <a href="/cart" className="text-gray-700 dark:text-white mr-5">
+              <span className="text-2xl bi bi-cart-fill relative">
+                <span className="text-white text-sm absolute -top-1 left-2.5 bg-rose-500 px-1 rounded-full ">
+                  0
+                </span>
+              </span>
+            </a>
+            <span className="bi bi-person-circle cursor-pointer text-2xl text-gray-700 dark:text-white"></span>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
---
+++ tailwind.config.js
@@ -0,0 +1,50 @@
+
+
+
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      borderRadius: {
+        0: "0px",
+        5: "5px",
+      },
+      margin: {
+        10: "10px",
+        30: "30px",
+      },
+      spacing: {
+        5: "5px",
+        7: "7px",
+        10: "10px",
+      },
+      colors: {
+        myRed: "#ac0d14",
+        myRedHover: "#8e090f",
+        btnBlueLight: "#1C59AE",
+        btnBlueDark: "#19509D",
+      },
+      width: {
+        40: "40px",
+        150: "150",
+        350: "350px",
+      },
+      height: {
+        "h-18": "4.375rem",
+        100: "100px",
+        250: "250px",
+      },
+      maxWidth: {
+        150: "150px",
+        1200: "1200px",
+      },
+    },
+  },
+  plugins: [],
+};
+
