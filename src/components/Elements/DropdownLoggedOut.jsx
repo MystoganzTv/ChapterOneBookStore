b3ddee3810efd79cc11f3fb3de3edaf7ec2d17bf
Enrique Padron Spångberg <enrique.padron853@gmail.com>
2023-11-27 15:17:02 -0500

    Dropdown
    Toggle Dropdown menu

--- src/components/Elements/DropdownLoggedOut.jsx
+++ src/components/Elements/DropdownLoggedOut.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 
-export const DropdownLoggedOut = () => {
+export const DropdownLoggedOut = ({ setDropdown }) => {
   return (
     <div
       id='dropdownAvatar'
@@ -10,6 +10,7 @@ export const DropdownLoggedOut = () => {
         aria-labelledby='dropdownUserAvatarButton'>
         <li>
           <Link
+            onClick={() => setDropdown(false)}
             to='/products'
             className='block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white'>
             All eBooks
@@ -17,6 +18,7 @@ export const DropdownLoggedOut = () => {
         </li>
         <li>
           <Link
+            onClick={() => setDropdown(false)}
             to='/login'
             className='block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white'>
             Login
@@ -24,6 +26,7 @@ export const DropdownLoggedOut = () => {
         </li>
         <li>
           <Link
+            onClick={() => setDropdown(false)}
             to='/register'
             className='block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white'>
             Register
