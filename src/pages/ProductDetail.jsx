282cda3fe8a0308b5f3524601a01abc481df2b1c
Enrique Padron Spångberg <enrique.padron853@gmail.com>
2023-12-22 14:08:57 -0500

    out of stock
    disabled button when out of stock

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -79,7 +79,10 @@ export const ProductDetail = () => {
               {!inCart && (
                 <button
                   onClick={() => addToCart(product)}
-                  className={`inline-flex items-center py-2 px-5 text-lg font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800`}>
+                  className={`inline-flex items-center py-2 px-5 text-lg font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 ${
+                    product.in_stock ? '' : 'cursor-not-allowed'
+                  }`}
+                  disabled={product.in_stock ? '' : 'disabled'}>
                   Add To Cart <i className='ml-1 bi bi-plus-lg'></i>
                 </button>
               )}
